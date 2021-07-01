b43ce52b4add7655da7566031126cb1937fc0645
EmyAs <40670283+EmyAs@users.noreply.github.com>
2021-07-01 20:36:41 +0200

    changes

---
+++ dogapp/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Dog />
+  </div>
+</template>
+
+<script>
+import Dog from "@/components/Dog";
+export default {
+  name: "app",
+  components: { Dog }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
